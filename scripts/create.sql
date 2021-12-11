fd99743b4b12e15e50503449ef94cea4df8a554a
walter <1654226880301@ingenieria.usac.edu.gt>
2021-12-11 15:55:01 -0600

    example

---
+++ scripts/create.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Banco
+(
+  IdBanco integer NOT NULL AUTO_INCREMENT,
+  Banco varchar(30),
+  CONSTRAINT IdBanco PRIMARY KEY (IdBanco)
+);
+
+CREATE TABLE BancoPerfil
+(
+  idBanco integer,
+  idPerfil integer
+);
+
+CREATE TABLE PerfilFinanciero
+(
+  IdPerfil integer NOT NULL AUTO_INCREMENT,
+  Activo float,
+  Pasivo float,
+  Patrimonio float,
+  fecha datetime,
+  CONSTRAINT IdPerfil PRIMARY KEY (IdPerfil)
+);
+
+ALTER TABLE BancoPerfil ADD CONSTRAINT FK_BancoPerfil_
+  FOREIGN KEY (idBanco) REFERENCES Banco (IdBanco);
+
+ALTER TABLE BancoPerfil ADD CONSTRAINT FK_BancoPerfil_1
+  FOREIGN KEY (idPerfil) REFERENCES PerfilFinanciero (IdPerfil);
